b657461f79d2fd773da2d83fb4a57f9af7854c6a
VentGrey <omarpurataf@gmail.com>
2022-12-30 18:12:12 -0600

    Mejoras al JS de la navbar

--- navbar.js
+++ navbar.js
@@ -1,21 +1,18 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) 
-    {
-      elmnt = z[i];
-      file = elmnt.getAttribute("inc");
-      if (file) 
-      {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() 
-        {
-          if (this.readyState == 4) 
-          {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+    const z = document.getElementsByTagName("*");
+    for (let i = 0; i < z.length; i++) {
+      const elmnt = z[i];
+      const file = elmnt.getAttribute("inc");
+      if (file) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) { elmnt.innerHTML = this.responseText; }
             elmnt.removeAttribute("inc");
           }}      
         xhttp.open("GET", file, true);
         xhttp.send();
         return;
-      }}};
+      }
+    }
+};
